869eabca7e1076f0a818c766525d0900f4ad7ef2
Dana Mulyana <danabontot@gmail.com>
2023-05-23 15:06:06 +0700

    Refactor Halaman Tambah dan Edit Record dengan Lit Component

--- src/js/pages/transactions/edit.js
+++ src/js/pages/transactions/edit.js
@@ -1,9 +1,31 @@
 const Edit = {
     async init() {
+      this._initialUI();
       await this._initialData();
       this._initialListener();
     },
    
+    _initialUI() {
+      const listInputRadioTransactionType = [
+        {
+          inputId: 'recordType1',
+          value: 'income',
+          caption: 'Pemasukan',
+          required: true,
+        },
+        {
+          inputId: 'recordType2',
+          value: 'expense',
+          caption: 'Pengeluaran',
+          required: true,
+        },
+      ];
+      const inputRadioTransactionTypeEdit = document.querySelector('#inputRadioTransactionTypeEdit');
+      inputRadioTransactionTypeEdit.setAttribute(
+        'listRadio',
+        JSON.stringify(listInputRadioTransactionType),
+      );
+    },
    
     async _initialData() {
       const transactionId = Number(this._getTransactionId());
@@ -28,12 +50,6 @@ const Edit = {
    
    
     _initialListener() {
-      const evidenceInput = document.querySelector('#validationCustomEvidence');
-      evidenceInput.addEventListener('change', () => {
-        this._updatePhotoPreview();
-      });
-   
-   
       const editRecordForm = document.querySelector('#editRecordForm');
       editRecordForm.addEventListener(
         'submit',
@@ -83,56 +99,38 @@ const Edit = {
       };
     },
    
-   
-    _updatePhotoPreview() {
-      const evidenceImg = document.querySelector('#validationCustomEvidenceImg');
-      const evidenceImgChange = document.querySelector('#validationCustomEvidenceImgChange');
-      const evidenceImgInput = document.querySelector('#validationCustomEvidence');
-   
-   
-      const photo = evidenceImgInput.files[0];
-      if (!photo) return;
-   
-   
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        evidenceImg.classList.add('d-none');
-        evidenceImgChange.classList.remove('d-none');
-   
-   
-        evidenceImgChange.style.backgroundImage = `url('${event.target.result}')`;
-      };
-   
-   
-      reader.readAsDataURL(photo);
-    },
-   
-   
     _populateTransactionToForm(transactionRecord = null) {
       if (!(typeof transactionRecord === 'object')) {
         throw new Error(`Parameter transactionRecord should be an object. The value is ${transactionRecord}`);
       }
    
-   
       const nameInput = document.querySelector('#validationCustomRecordName');
       const amountInput = document.querySelector('#validationCustomAmount');
       const dateInput = document.querySelector('#validationCustomDate');
-      const evidenceInput = document.querySelector('#validationCustomEvidenceImg');
+      const inputImagePreviewEdit = document.querySelector('#inputImagePreviewEdit');
       const descriptionInput = document.querySelector('#validationCustomNotes');
-      const types = document.querySelectorAll('input[name="recordType"]');
-   
-   
+      const inputRadioTransactionTypeEdit = document.querySelector('#inputRadioTransactionTypeEdit');
+
       nameInput.value = transactionRecord.name;
       amountInput.value = transactionRecord.amount;
       dateInput.value = transactionRecord.date;
-      evidenceInput.setAttribute('src', transactionRecord.evidenceUrl);
-      evidenceInput.setAttribute('alt', transactionRecord.name);
+
+      inputImagePreviewEdit.setAttribute('defaultImage', transactionRecord.evidenceUrl);
+      inputImagePreviewEdit.setAttribute('defaultImageAlt', transactionRecord.name);
+ 
       descriptionInput.value = transactionRecord.description;
-      types.forEach((item) => {
+
+      const listInputRadioTransactionType = JSON.parse(
+        inputRadioTransactionTypeEdit.getAttribute('listRadio'),
+      );
+      listInputRadioTransactionType.forEach((item) => {
         item.checked = item.value === transactionRecord.type;
       });
+      inputRadioTransactionTypeEdit.setAttribute(
+        'listRadio',
+        JSON.stringify(listInputRadioTransactionType),
+      );
     },
-   
    
     _validateFormData(formData) {
       delete formData.evidence;
